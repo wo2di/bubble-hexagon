dfe8fd84d44e69b3348f3156a88b5c0928547f6d
DESKTOP-TK1EMG0\hbhun <hbhuni@gmail.com>
2022-03-03 03:08:09 +0900

    .

---
+++ BubbleHexagon/Assets/GooglePlayTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GooglePlayTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
