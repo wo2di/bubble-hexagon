d48a04d7cc5fb31904c3e549c55135bd97c6f0db
jaee <dw031251@gmail.com>
2022-03-16 16:21:02 +0900

    random 수정중

--- BubbleHexagon/Assets/Bubble/Behaviour/BubbleBHRandom.cs
+++ BubbleHexagon/Assets/Bubble/Behaviour/BubbleBHRandom.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class BubbleBHRandom : BubbleBehaviour
 {
     BubbleFactory factory;
     public Sound randomSound;
+    public List<BubbleBHColor> adjacentColorBubbles;
     protected override void Awake()
     {
         base.Awake();
@@ -15,20 +17,23 @@ public class BubbleBHRandom : BubbleBehaviour
     public override void OnSetToSlot()
     {
         randomSound.source.Play();
+        
+        var result = from b in bubble.slot.GetAdjacentBubbles()
+                     where b.GetComponent<BubbleBehaviour>() is BubbleBHColor
+                     select b.GetComponent<BubbleBHColor>();
+        adjacentColorBubbles = result.ToList();
+
         StartCoroutine(ChangeAdjacentColors());
     }
 
     public IEnumerator ChangeAdjacentColors()
     {
         yield return new WaitForSeconds(0.1f);
-        foreach (Bubble b in bubble.slot.GetAdjacentBubbles())
+
+        foreach (BubbleBHColor c in adjacentColorBubbles)
         {
-            switch (b.GetComponent<BubbleBehaviour>())
-            {
-                case BubbleBHColor behavColor:
-                    behavColor.ChangeToNewColor();
-                    break;
-            }
+            c.ChangeToNewColor();
+            c.OnSetToSlot();
         }
         OnPop();
     }
